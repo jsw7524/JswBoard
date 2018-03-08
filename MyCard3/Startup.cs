f7ce71bd9654fa5dff6bbc03cf70e2d558a3d9c9
jsw7524 <jsw7524@gmail.com>
2018-03-08 23:42:43 +0800

    Add project files.

---
+++ MyCard3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyCard3.Startup))]
+namespace MyCard3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
